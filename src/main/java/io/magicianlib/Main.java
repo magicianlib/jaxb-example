9a84088bad5ddc79f2b43b6ab44cd324eece5fb5
magicianlib <magicianlib@gmail.com>
2023-10-18 14:29:17 +0800

    update

--- src/main/java/io/magicianlib/Main.java
+++ src/main/java/io/magicianlib/Main.java
@@ -18,10 +18,12 @@ public class Main {
         List<Property> properties = new ArrayList<>();
         properties.add(new Property("username", "Username", "java.lang.String"));
         properties.add(new Property("deleted", "Deleted", "java.lang.Integer"));
+        properties.add(new Property());
 
         List<Table> tables = new ArrayList<>();
         tables.add(new Table("order_paid_receipt_config", "OrderPaidReceiptConfig", properties));
         tables.add(new Table("order_flight", "OrderFlight")); // no properties
+        tables.add(new Table()); // no properties
 
         GeneratorConfiguration configuration = new GeneratorConfiguration();
         configuration.setTables(tables);
@@ -32,16 +34,16 @@ public class Main {
             JAXBContext context = JAXBContext.newInstance(GeneratorConfiguration.class);
             Marshaller marshaller = context.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true); // 格式化输出
-            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, false); // 禁用 standalone 声明
-            marshaller.setProperty(Marshaller.JAXB_ENCODING, false); // 设置生成的 XML 的字符编码
+            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, false);        // 不生成XML文档的声明部分 <?xml version="1.0"?>
+            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");      // 设置生成的 XML 的字符编码
 
-            // 直接输出到控制台
             System.out.println("marshal XML:");
             marshaller.marshal(configuration, writer);
             xml = writer.toString();
             System.out.println(writer);
 
             // or
+            // 直接输出到控制台
             // marshaller.marshal(configuration, System.out);
 
         } catch (Exception e) {
